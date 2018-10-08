826b6f8dd5954851ba6ffac0234f1100203f9d1c
the-ashu <ashutosh16215@gmail.com>
2018-10-09 00:09:00 +0530

    first commit

---
+++ application/libraries/Google.php
@@ -0,0 +1,18 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+|  Google API Configuration
+| -------------------------------------------------------------------
+|  client_id         string   Your Google API Client ID.
+|  client_secret     string   Your Google API Client secret.
+|  redirect_uri      string   URL to redirect back to after login.
+|  application_name  string   Your Google application name.
+|  api_key           string   Developer key.
+|  scopes            string   Specify scopes
+*/
+$config['google']['client_id']        = '223459385127-j3fml107ac6ro11kinsbtp32gicfetko.apps.googleusercontent.com';
+$config['google']['client_secret']    = 'Zo0JqdnrVw7PUbLjRBfVnZ2c';
+$config['google']['redirect_uri']     = 'http://localhost/codeigniter/user_authentication/';
+$config['google']['application_name'] = 'knit';
+$config['google']['api_key']          = 'AIzaSyB9y2ji-uuSrVdWustcoFZu5NM10hHAdPw';
+$config['google']['scopes']           = array();
